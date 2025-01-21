2c6a6dfd689daa322e5483fe3952ca9ca13e4402
Nate Stone <nate.stone@opendatarepository.org>
2025-01-21 13:56:48 -0500

    Trying to add IOS 16+ compatibility

--- ios/Plugin/InAppReview.swift
+++ ios/Plugin/InAppReview.swift
@@ -4,7 +4,44 @@ import StoreKit
 
 @objc public class InAppReview: NSObject {
     @objc func requestReview(_ call: CAPPluginCall) {
-        SKStoreReviewController.requestReview()
+        if #available(iOS 17.0, *) {
+            @Environment(\.requestReview) private var requestReview
+
+            private func presentReview() {
+                Task {
+                    // Delay for two seconds to avoid interrupting the person using the app.
+                    try await Task.sleep(for: .seconds(2))
+                    await requestReview()
+                }
+            }
+            
+            presentReview()
+
+            /*
+            if processCompletedCount >= 4, currentAppVersion != lastVersionPromptedForReview {
+                    
+                // The app already displayed the rating and review request view. Store this current version.
+                lastVersionPromptedForReview = currentAppVersion
+            }
+
+            var body: some View {
+                Button("Ask for Review") {
+                    DispatchQueue.main.async {
+                        requestReview()
+                    }
+                }
+            }
+             */
+        }
+        else if #available(iOS 14.0, *) {
+             if let windowScene = UIApplication.shared.connectedScenes.first(
+                where: { $0.activationState == .foregroundActive }
+             ) as? UIWindowScene {
+                 SKStoreReviewController.requestReview(in: windowScene)
+             }
+         } else {
+             SKStoreReviewController.requestReview()
+         }
         call.resolve()
     }
 }
